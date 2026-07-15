524a7019274c85c8a7764b20f0422edff2aecde9
Nadja Rhodes <narhodes1@gmail.com>
2026-07-14 21:07:43 -0300

    Use slate background for PWA icons (#94)

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    The stamp line below is rewritten by that script so this file's bytes
    change on every deploy (update detection). */
 /* global PRECACHE_VERSION, PRECACHE_URLS */
-// precache-version: a67bc03fabda
+// precache-version: f7823483c2f1
 importScripts('sw-precache.js');
 
 const CACHE_NAME = `fortudo-shell-${PRECACHE_VERSION}`;
